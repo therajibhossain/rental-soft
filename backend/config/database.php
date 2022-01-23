9859b79f8d7bc4c9dbb9e87dceee49365573cd71
Rajib Hossain <rajibhossain.php@gmail.com>
2022-01-23 17:16:04 +0600

    backend api service deployment

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://wxmqxnwlcsmwnk:45543a30c1e60fbc9b285853304051b6d3522af85502638cab6611124aee834d@ec2-18-234-17-166.compute-1.amazonaws.com:5432/df3da7dgs7k64k');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,15 +67,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
+			'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const token = "3|tjSI5alcZwVewWA1iMdOkz02K4UMe0KRmDy2uuEn";
+const token = "1|7JM0TfMkou4fON3cW8oBXKHtGW8UHqJapNp3kR4r";
 
 export default axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: "https://rental-soft.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
     Authorization: `Bearer ${token}`
